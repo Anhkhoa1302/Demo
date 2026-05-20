1b878fa765b6ca186742ea8332862ff65a2fb3e5
Anhkhoa1302 <tranmacanhkhoa1302@gmail.com>
2026-05-20 21:33:41 +0700

    Create index.java

--- index.java
+++ index.java
@@ -2,6 +2,10 @@ package Demo;
 
 public class index {
 public static void main(String[] args) {
-    System.out.println("Hello !");
+    System.out.println("Hello");
+    int a = 10;
+    int b = 20;
+    int sum = a+b ;
+    System.out.println("Tong cua a va b: "+ sum);
 }
 }

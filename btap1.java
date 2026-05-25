3d064692139a7b8bb5c69d3f56df4292f9b9a04c
Anhkhoa1302 <tranmacanhkhoa1302@gmail.com>
2026-05-25 20:43:55 +0700

    Create btap1

---
+++ btap1.java
@@ -0,0 +1,13 @@
+package Demo;
+import java.util.Scanner;
+public class btap1 {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in); 
+System.out.println("Nhap so a: ");
+    int a = sc.nextInt();
+    System.out.println("Nhap so b: ");
+    int b = sc.nextInt();
+sc.nextLine();
+System.out.println(a+b);
+}    
+}

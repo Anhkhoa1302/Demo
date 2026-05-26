e83251c21bc9c26836b5b0b3ed1c78428f796eec
Anhkhoa1302 <tranmacanhkhoa1302@gmail.com>
2026-05-27 00:03:12 +0700

    Create so fibonacci

---
+++ sofibonacci.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class sofibonacci {
+    public static void main(String[] args) {
+        int i = 0;
+        int a = 0;
+        int b = 1;
+        int c ;
+        while (i < 15) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+            i++;
+        }
+
+    }
+}
